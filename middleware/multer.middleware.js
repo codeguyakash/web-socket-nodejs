6cec17662424ed71e7f484a3441aacc77ba301ea
Akash <codeguyakash@gmail.com>
2024-12-18 00:33:20 +0530

    Enhance multer middleware to create uploads directory if it doesn't exist

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,17 +1,24 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
+const uploadDir = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+    console.log("📁 Uploads folder created automatically.");
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../uploads'));
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         console.log(file);
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
         const fileExtension = path.extname(file.originalname);
         cb(null, `screenshot-${uniqueSuffix}${fileExtension}`);
-    }
+    },
 });
 
 const upload = multer({ storage: storage });
