ba99798704b2df46f10bc372a34ad6828fa7c3ab
Akash <codeguyakash@gmail.com>
2024-12-18 18:09:41 +0530

    Add screenshot capture and retrieval endpoints; enhance upload handling

--- server.js
+++ server.js
@@ -5,23 +5,24 @@ const multer = require('multer');
 const path = require('path');
 const WebSocket = require('ws');
 const connectDB = require('./db/database');
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
-let databaseInstance;
 const server = http.createServer(app);
+let isUploadedImage = false;
 
 const wss = new WebSocket.Server({ server });
 
 let pool;
 
 (async () => {
-  try {
-    pool = await connectDB();
-  } catch (error) {
-    console.error("Failed to initialize database connection", error.message);
-    process.exit(1);
-  }
+    try {
+        pool = await connectDB();
+    } catch (error) {
+        console.error("Failed to initialize database connection", error.message);
+        process.exit(1);
+    }
 })();
 
 console.log(`WebSocket and Express server running on http://localhost:${port}`);
@@ -33,7 +34,6 @@ function broadcast(message) {
         }
     });
 }
-console.log("object");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -47,9 +47,12 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, req) => {
     console.log('Client connected');
     ws.send('Welcome to the WebSocket server!');
+
+
+    // console.log(req);
 
     ws.on('message', (message) => {
         console.log(`Received from client: ${message}`);
@@ -57,6 +60,9 @@ wss.on('connection', (ws) => {
             console.log('Sending screenshot capture instruction');
             ws.send('capture_screenshot');
         }
+    });
+    ws.on("screenshot_uploaded", (message) => {
+        console.log('Screenshot Captured Acknowledged', message);
     });
 
     ws.on('close', () => {
@@ -68,31 +74,63 @@ wss.on('connection', (ws) => {
     });
 });
 
-app.get("/", async (_, res)  => {
+app.get("/", async (_, res) => {
     try {
         const [rows] = await pool.query("SELECT * FROM device");
         res.status(200).json({ success: true, data: rows });
-      } catch (error) {
+    } catch (error) {
         console.error("Error executing query:", error.message);
         res.status(500).json({ error: "Database query failed." });
-      }
+    }
 });
 
-app.get('/capture_screenshot', (req, res) => {
-    console.log('API endpoint hit: Triggering screenshot capture');
-    broadcast('capture_screenshot');
-    res.status(200).json({ message: 'Screenshot capture instruction sent to clients.' });
+app.post("/reboot", async (req, res) => {
+
+    broadcast(`reboot`);
+    res.status(200).json({ message: `Rebooting device` });
 });
+
+
+app.get('/capture_screenshot', async (req, res) => {
+    console.log('Triggering screenshot capture');
+    broadcast('capture_screenshot');
+    res.status(200).json({ message: 'Screenshot capture triggered' });
+});
+app.get('/get_screenshot', async (req, res) => {
+    console.log('Requesting screenshot');
+
+    try {
+        const file = await getFileAfterUpload();
+        const path = `http://localhost:${port}/uploads/${file}`;
+        res.status(200).json({ message: 'Screenshot requested', fileName: path });
+    } catch (error) {
+        console.error('Error getting screenshot:', error);
+        res.status(500).json({ message: 'Internal server error', error });
+    }
+
+});
+app.get("/uploads/:file", (req, res) => {
+    const file = req.params.file;
+    const filePath = path.join(__dirname, '/uploads', file);
+    res.sendFile(filePath);
+});
+
+
+getFileAfterUpload = async () => {
+    const files = fs.readdirSync(path.join(__dirname, '/uploads'));
+    const file = files[files.length - 1];
+    return file;
+}
 
 app.post('/upload', upload.single('file'), (req, res) => {
     try {
         const file = req.file;
-
         if (!file) {
             return res.status(400).json({ message: 'No file uploaded' });
         }
 
         console.log(`File uploaded: ${file.filename}`);
+        isUploadedImage = true;
         res.status(200).json({
             message: 'File uploaded successfully',
             fileName: file.filename,
